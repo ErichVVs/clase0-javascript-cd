81e0c8b86208c0c799b746df95e6d089fc08c226
erichvvs <erich.adan@gmail.com>
2022-07-05 20:45:28 -0500

    ejercicios curp

--- Clase 1/main2.js
+++ Clase 1/main2.js
@@ -1,3 +1,13 @@
 let nombre = "Erich";
 let apellido = "Vivas"
 console.log (nombre + " " + apellido);
+
+let letras1erapellido = "VI";
+let letras2doapellido = "SA";
+let aniodenacimiento = "94";
+let mesdenacimiento = "10";
+let diadenacimiento = "01";
+let sexo = "H";
+let ciudadnacimiento = "DF";
+let claverfc = "FNA";
+console.log (letras1erapellido + letras2doapellido + aniodenacimiento +  mesdenacimiento + diadenacimiento + sexo + ciudadnacimiento + claverfc)
